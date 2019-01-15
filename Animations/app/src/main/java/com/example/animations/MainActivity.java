caf2ad01aa002af70d256f6655dfdf0b92e87a54
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-15 17:40:14 -0600

    animated gif

---
+++ Animations/app/src/main/java/com/example/animations/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.animations;
+
+import android.graphics.ImageDecoder;
+import android.graphics.drawable.AnimatedImageDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView imageView = findViewById(R.id.animated_image_view);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final Drawable drawable1 = ImageDecoder.decodeDrawable(ImageDecoder.createSource(getResources(), R.drawable.android_project));
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImageDrawable(drawable1);
+                        }
+                    });
+                    ((AnimatedImageDrawable)drawable1).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
