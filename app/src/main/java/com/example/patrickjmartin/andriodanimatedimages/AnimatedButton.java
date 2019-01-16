07c6420de050136fdd129822938ca94ae64e3ce1
Patrick Martin <patrickmartin88@gmail.com>
2019-01-16 00:32:52 -0800

    Altered so that button layout looks better. Not really a button but a very pretty animation.

--- app/src/main/java/com/example/patrickjmartin/andriodanimatedimages/AnimatedButton.java
+++ app/src/main/java/com/example/patrickjmartin/andriodanimatedimages/AnimatedButton.java
@@ -1,8 +1,11 @@
 package com.example.patrickjmartin.andriodanimatedimages;
 
 import android.content.Context;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,11 +13,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import static com.example.patrickjmartin.andriodanimatedimages.R.drawable.time_switcher_anim_12;
+
 
 public class AnimatedButton extends Fragment {
 
     private ImageView animatedButtonVector;
     private static String title = "Animated Vector";
+    private boolean isNight = true;
 
     public AnimatedButton() {
         // Required empty public constructor
@@ -38,7 +44,40 @@ public class AnimatedButton extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        animatedButtonVector = view.findViewById(R.id.animated_button_imageView);
+    }
+
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        animatedButtonVector.setImageDrawable(getActivity()
+                .getDrawable(R.drawable.time_switcher_anim_13));
+        final Drawable drawable = animatedButtonVector.getDrawable();
+        if(drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
+
+        animatedButtonVector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isNight) {
+                    animatedButtonVector.setImageDrawable(getActivity()
+                            .getDrawable(time_switcher_anim_12));
+                    isNight = false;
+                } else {
+                    animatedButtonVector.setImageDrawable(getActivity()
+                            .getDrawable(R.drawable.time_switcher_anim_13));
+                    isNight = true;
+                }
+                final Drawable drawable = animatedButtonVector.getDrawable();
+                if(drawable instanceof Animatable) {
+                    ((Animatable) drawable).start();
+                }
+
+            }
+        });
     }
 }
