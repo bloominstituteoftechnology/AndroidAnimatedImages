d8cfee3cd0a5103e2538735aaa87509f58dee037
Patrick Martin <patrickmartin88@gmail.com>
2019-01-15 16:07:07 -0800

    EOD commit

--- app/src/main/java/com/example/patrickjmartin/andriodanimatedimages/MainActivity.java
+++ app/src/main/java/com/example/patrickjmartin/andriodanimatedimages/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.patrickjmartin.andriodanimatedimages;
 import android.graphics.ImageDecoder;
 import android.graphics.drawable.AnimatedImageDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -11,36 +13,46 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    FragmentPagerAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView imageView = findViewById(R.id.animated_imageView);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        adapter = new ViewPageAdapter(getSupportFragmentManager());
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    final Drawable drawable = ImageDecoder.
-                            decodeDrawable(ImageDecoder.createSource(getResources(), R.drawable.fakerealcat));
 
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            imageView.setImageDrawable(drawable);
-                        }
-                    });
-                    if(drawable instanceof AnimatedImageDrawable) {
-                        ((AnimatedImageDrawable) drawable).start();
-                    }
 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
 
 
     }
 }
+
+//        final ImageView imageView = findViewById(R.id.animated_imageView);
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    final Drawable drawable = ImageDecoder.
+//                            decodeDrawable(ImageDecoder.createSource(getResources(), R.drawable.fakerealcat));
+//
+//                    runOnUiThread(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            imageView.setImageDrawable(drawable);
+//                        }
+//                    });
+//                    if(drawable instanceof AnimatedImageDrawable) {
+//                        ((AnimatedImageDrawable) drawable).start();
+//                    }
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
+
--- app/src/main/java/com/example/patrickjmartin/andriodanimatedimages/ViewPageAdapter.java
+++ app/src/main/java/com/example/patrickjmartin/andriodanimatedimages/ViewPageAdapter.java
@@ -7,16 +7,29 @@ import android.support.v4.app.FragmentPagerAdapter;
 import java.util.HashMap;
 
 public class ViewPageAdapter extends FragmentPagerAdapter {
-    private final HashMap<String, Fragment> fragments = new HashMap<>();
+
     private static int NUM_ITEMS = 3;
 
     @Override
     public Fragment getItem(int i) {
-        return null;
+        switch (i) {
+            case 0:
+                return AnimatedGIF.newInstance(0, "Page # 1");
+            case 1: // Fragment # 0 - This will show FirstFragment different title
+                return FirstFragment.newInstance(1, "Page # 2");
+            case 2: // Fragment # 1 - This will show SecondFragment
+                return SecondFragment.newInstance(2, "Page # 3");
+            default:
+                return null;
+        }
     }
 
     public ViewPageAdapter(FragmentManager fragmentManager) {
         super(fragmentManager);
+    }
+
+    public void addFragment(String key, Fragment value) {
+        fragments.put(key, value);
     }
 
     @Override
