bc6076267403daa24ae01496f33563ef2b59c2c6
JTyzz <jtyzzer@gmail.com>
2019-01-29 08:01:06 -0800

    Finished part 3

--- app/src/main/java/com/earthdefensesystem/androidanimatedimages/MainActivity.java
+++ app/src/main/java/com/earthdefensesystem/androidanimatedimages/MainActivity.java
@@ -1,13 +1,43 @@
 package com.earthdefensesystem.androidanimatedimages;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView animatedButton;
+    private boolean isClicked = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        animatedButton = findViewById(R.id.image_button);
+
+        animatedButton.setImageDrawable(getDrawable(R.drawable.switch_vector_1));
+        final Drawable drawable = (animatedButton).getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
+
+        animatedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isClicked) {
+                    animatedButton.setImageDrawable(getDrawable(R.drawable.switch_vector_2));
+                    isClicked = false;
+                } else {
+                    animatedButton.setImageDrawable(getDrawable(R.drawable.switch_vector_1));
+                    isClicked = true;
+                }
+                final Drawable drawable = animatedButton.getDrawable();
+                if (drawable instanceof Animatable) {
+                    ((Animatable) drawable).start();
+                }
+            }
+        });
     }
 }
