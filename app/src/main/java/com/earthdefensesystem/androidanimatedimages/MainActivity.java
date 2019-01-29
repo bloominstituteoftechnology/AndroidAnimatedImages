a8e2e419c78d469501f5d6b3fe7b83699ca7c711
JTyzz <jtyzzer@gmail.com>
2019-01-29 08:25:54 -0800

    Finished part 2

--- app/src/main/java/com/earthdefensesystem/androidanimatedimages/MainActivity.java
+++ app/src/main/java/com/earthdefensesystem/androidanimatedimages/MainActivity.java
@@ -1,14 +1,17 @@
 package com.earthdefensesystem.androidanimatedimages;
 
+import android.content.Intent;
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView animatedButton;
+    private Button nextActivity;
     private boolean isClicked = true;
 
     @Override
@@ -16,12 +19,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         animatedButton = findViewById(R.id.image_button);
+        nextActivity = findViewById(R.id.button);
+
 
         animatedButton.setImageDrawable(getDrawable(R.drawable.switch_vector_1));
         final Drawable drawable = (animatedButton).getDrawable();
         if (drawable instanceof Animatable) {
             ((Animatable) drawable).start();
         }
+        
+        nextActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),ListVector.class);
+                startActivity(i);
+            }
+        });
 
         animatedButton.setOnClickListener(new View.OnClickListener() {
             @Override
