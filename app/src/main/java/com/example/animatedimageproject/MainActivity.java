17dd8928b7fc673c6fe03ddc4f828dd61f9c2017
BL-CoderOvertime <brandonlively@live.com>
2019-04-09 17:24:36 -0400

    setup the layout file and declared views in main activity class

--- app/src/main/java/com/example/animatedimageproject/MainActivity.java
+++ app/src/main/java/com/example/animatedimageproject/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.animatedimageproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView playButton;
+    ImageView gifView;
+    Spinner gifSelector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gifSelector = findViewById(R.id.gif_selector);
+        playButton = findViewById(R.id.play_gif_button);
+        gifView = findViewById(R.id.imageView_gif);
     }
 }
