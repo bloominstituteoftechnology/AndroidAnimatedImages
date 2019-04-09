0d37353757726667bfa246fc445bac673beaf7d8
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-04-09 15:35:10 -0400

    Designed layout and added a gif

--- AnimatedImages/app/src/main/java/com/rybarstudios/animatedimages/MainActivity.java
+++ AnimatedImages/app/src/main/java/com/rybarstudios/animatedimages/MainActivity.java
@@ -1,13 +1,23 @@
 package com.rybarstudios.animatedimages;
 
+import android.graphics.drawable.AnimatedImageDrawable;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.image_view);
+        mImageView.setImageDrawable(getDrawable(R.drawable.space));
+        ((AnimatedImageDrawable)mImageView.getDrawable()).start();
+
     }
 }
