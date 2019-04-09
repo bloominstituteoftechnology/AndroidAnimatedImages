832e29b6a076f61af43c3416b5a7db6a400f0076
Jake Esveld <thejakeesveld@gmail.com>
2019-04-09 14:50:36 -0700

    Changed AnimatedVectorDrawable and added toggle functionality

--- app/src/main/java/com/jakeesveld/android_animatedimages/MainActivity.java
+++ app/src/main/java/com/jakeesveld/android_animatedimages/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.drawable.AnimatedImageDrawable;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     RadioButton buttonGif;
     RadioButton buttonAnimation;
     RadioButton buttonVector;
+    ConstraintLayout parentLayout;
+    Boolean pickup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +29,13 @@ public class MainActivity extends AppCompatActivity {
         buttonAnimation = findViewById(R.id.button_animation);
         buttonGif = findViewById(R.id.button_gif);
         buttonVector = findViewById(R.id.button_vector);
+        parentLayout = findViewById(R.id.parent_layout);
 
         imageView.setImageDrawable(getDrawable(R.drawable.snoop_dogg));
+        parentLayout.setBackground(getDrawable(R.drawable.compton_background));
         buttonGif.setChecked(true);
         refreshAnimation();
+        pickup = false;
 
         buttonGif.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 buttonAnimation.setChecked(false);
                 buttonVector.setChecked(false);
                 imageView.setImageDrawable(getDrawable(R.drawable.snoop_dogg));
+                parentLayout.setBackground(getDrawable(R.drawable.compton_background));
                 refreshAnimation();
             }
         });
@@ -49,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 buttonVector.setChecked(false);
                 buttonGif.setChecked(false);
                 imageView.setImageDrawable(getDrawable(R.drawable.goat));
+                parentLayout.setBackground(getDrawable(R.drawable.pasture_background));
                 refreshAnimation();
             }
         });
@@ -59,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 buttonGif.setChecked(false);
                 buttonVector.setChecked(true);
                 buttonAnimation.setChecked(false);
-                imageView.setImageDrawable(getDrawable(R.drawable.avd_anim_get_up));
+                imageView.setImageDrawable(getDrawable(R.drawable.avd_anim_ring_pickup));
                 refreshAnimation();
             }
         });
@@ -68,14 +76,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void refreshAnimation(){
-        Drawable drawable = imageView.getDrawable();
+        final Drawable drawable = imageView.getDrawable();
         if(drawable instanceof AnimatedImageDrawable){
             ((AnimatedImageDrawable) drawable).start();
         }else if(drawable instanceof AnimationDrawable){
             ((AnimationDrawable) drawable).start();
         }else if(drawable instanceof AnimatedVectorDrawable){
             ((AnimatedVectorDrawable) drawable).start();
+            pickup = !pickup;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
+                    if(pickup){
+                        imageView.setImageDrawable(getDrawable(R.drawable.avd_anim_pickup_ring));
+                    }else{
+                        imageView.setImageDrawable(getDrawable(R.drawable.avd_anim_ring_pickup));
+                    }
+                    Drawable current = imageView.getDrawable();
+                    ((AnimatedVectorDrawable) current).start();
+                    pickup = !pickup;
+                }
+            });
         }
     }
 
