24c227c1c19d5c3a142abbe1aa25f59042b2f038
heiligbasil <basilief@yahoo.com>
2019-04-09 22:15:40 -0700

    Replaced stock BottomNavigationView icons and animated starts & stops

--- app/src/main/java/com/lambdaschool/androidanimatedimages/MainActivity.java
+++ app/src/main/java/com/lambdaschool/androidanimatedimages/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.androidanimatedimages;
 
 import android.graphics.drawable.AnimatedImageDrawable;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -8,21 +9,22 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AnimatedImageDrawable animatedImageDrawable;
+    private BottomNavigationView bottomNavigationView;
     private AnimationDrawable animationDrawable;
     private Drawable drawable;
 
+    private boolean buttonAnimationEnded;
     private TextView mTextMessage;
     private ImageView imageView;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
@@ -38,26 +40,29 @@ public class MainActivity extends AppCompatActivity {
             }
 
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_gif:
                     mTextMessage.setText(R.string.title_gif);
                     imageView.setImageDrawable(getDrawable(R.drawable.starblown));
                     drawable = imageView.getDrawable();
                     animatedImageDrawable = (AnimatedImageDrawable) drawable;
-                    animatedImageDrawable.start();
+                    if (buttonAnimationEnded)
+                        animatedImageDrawable.start();
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_avd:
                     mTextMessage.setText(R.string.title_avd);
                     imageView.setImageDrawable(getDrawable(R.drawable.coyote));
                     drawable = imageView.getDrawable();
                     animationDrawable = (AnimationDrawable) drawable;
-                    animationDrawable.start();
+                    if (buttonAnimationEnded)
+                        animationDrawable.start();
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_gif2:
                     mTextMessage.setText(R.string.title_gif2);
                     imageView.setImageDrawable(getDrawable(R.drawable.gomez_addams_trampoline));
                     drawable = imageView.getDrawable();
                     animatedImageDrawable = (AnimatedImageDrawable) drawable;
-                    animatedImageDrawable.start();
+                    if (buttonAnimationEnded)
+                        animatedImageDrawable.start();
                     return true;
             }
             return false;
@@ -70,14 +75,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.image_view);
-        drawable = imageView.getDrawable();
-        animatedImageDrawable = (AnimatedImageDrawable) drawable;
-        animatedImageDrawable.start();
-
         mTextMessage = findViewById(R.id.text_view);
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        buttonAnimationEnded = false;
+        final ImageView imageViewButton = findViewById(R.id.image_view_button);
+        imageViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (buttonAnimationEnded) {
+                    imageViewButton.setImageDrawable(getDrawable(R.drawable.line_forms_circle));
+
+                } else {
+                    imageViewButton.setImageDrawable(getDrawable(R.drawable.circle_melts_line));
+                }
+                drawable = imageViewButton.getDrawable();
+                ((AnimatedVectorDrawable) drawable).start();
+                buttonAnimationEnded = !buttonAnimationEnded;
+
+                drawable = imageView.getDrawable();
+                if (drawable instanceof AnimatedImageDrawable) {
+                    animatedImageDrawable = (AnimatedImageDrawable) drawable;
+                    if (animatedImageDrawable.isRunning())
+                        animatedImageDrawable.stop();
+                    else
+                        bottomNavigationView.setSelectedItemId(bottomNavigationView.getSelectedItemId());
+                } else if (drawable instanceof AnimationDrawable) {
+                    animationDrawable = (AnimationDrawable) drawable;
+                    if (animationDrawable.isRunning())
+                        animationDrawable.stop();
+                    else
+                        bottomNavigationView.setSelectedItemId(bottomNavigationView.getSelectedItemId());
+                }
+
+            }
+        });
     }
-
-
 }
