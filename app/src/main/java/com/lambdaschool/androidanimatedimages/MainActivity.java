d1d682e56fd2e3154054fb6cb8f2f8766a853d89
heiligbasil <basilief@yahoo.com>
2019-04-09 17:27:40 -0700

    Added animation logic for the two gifs and the AVD in the navigation

--- app/src/main/java/com/lambdaschool/androidanimatedimages/MainActivity.java
+++ app/src/main/java/com/lambdaschool/androidanimatedimages/MainActivity.java
@@ -1,5 +1,8 @@
 package com.lambdaschool.androidanimatedimages;
 
+import android.graphics.drawable.AnimatedImageDrawable;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,6 +13,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AnimatedImageDrawable animatedImageDrawable;
+    private AnimationDrawable animationDrawable;
+    private Drawable drawable;
+
     private TextView mTextMessage;
     private ImageView imageView;
 
@@ -18,15 +25,39 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            drawable = imageView.getDrawable();
+            if (drawable instanceof AnimatedImageDrawable) {
+                animatedImageDrawable = (AnimatedImageDrawable) drawable;
+                if (animatedImageDrawable.isRunning())
+                    animatedImageDrawable.stop();
+            } else if (drawable instanceof AnimationDrawable) {
+                animationDrawable = (AnimationDrawable) drawable;
+                if (animationDrawable.isRunning())
+                    animationDrawable.stop();
+            }
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_gif);
+                    imageView.setImageDrawable(getDrawable(R.drawable.starblown));
+                    drawable = imageView.getDrawable();
+                    animatedImageDrawable = (AnimatedImageDrawable) drawable;
+                    animatedImageDrawable.start();
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_avd);
+                    imageView.setImageDrawable(getDrawable(R.drawable.coyote));
+                    drawable = imageView.getDrawable();
+                    animationDrawable = (AnimationDrawable) drawable;
+                    animationDrawable.start();
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_gif2);
+                    imageView.setImageDrawable(getDrawable(R.drawable.gomez_addams_trampoline));
+                    drawable = imageView.getDrawable();
+                    animatedImageDrawable = (AnimatedImageDrawable) drawable;
+                    animatedImageDrawable.start();
                     return true;
             }
             return false;
@@ -39,10 +70,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.image_view);
+        drawable = imageView.getDrawable();
+        animatedImageDrawable = (AnimatedImageDrawable) drawable;
+        animatedImageDrawable.start();
 
-        mTextMessage = (TextView) findViewById(R.id.text_view);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = findViewById(R.id.text_view);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 
 }
