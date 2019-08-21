98ba74cd6f31b9a926f9415a91dda635e78aa7d8
Troy Christian <apostasy8@gmail.com>
2019-08-21 16:01:21 -0700

    add avd's (Pause-to_play) and (play-to-pause) into Drawable folder. Create avd_play_to_pause.xml, avd_pause_to_play.xml

--- app/src/main/java/com/example/androidanimatedimages/MainActivity.kt
+++ app/src/main/java/com/example/androidanimatedimages/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.androidanimatedimages
 
+import android.annotation.SuppressLint
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+ @SuppressLint("WrongThread")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+              val gifDrawable = ImageDecoder.decodeDrawable(ImageDecoder.createSource(resources, R.drawable.unicorn))
+            animated_image.setImageDrawable(gifDrawable)
+              (gifDrawable as AnimatedImageDrawable).start()
+
+        }
+     animated_image
+    }}
 }
