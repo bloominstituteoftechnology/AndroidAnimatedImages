52927fc8e338243b1c906a45f1f3eb169c945293
stilljack <stilljack@gmail.comconfig --global user.email stilljack@gmail.com>
2019-08-21 12:35:45 -0700

    3 fragments made to house our 3 different types of animations

---
+++ app/src/main/java/com/example/androidanimatedimages/acitivities/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.androidanimatedimages.acitivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidanimatedimages.R
+import com.example.androidanimatedimages.fragments.AnimatedDrawableFragment
+import com.example.androidanimatedimages.fragments.AnimatedVectorFragment
+import com.example.androidanimatedimages.fragments.GifFragment
+
+class MainActivity : AppCompatActivity() {
+
+    //i could probably make just one fragment that would determine which kind  of drawable is being passed to it, but we're gonna
+    //do it this way
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val gifFragment = GifFragment()
+        val animatedDrawableFragment = AnimatedDrawableFragment()
+        val animatedVectorFragment = AnimatedVectorFragment()
+
+    /*
+            if we need a bundle...
+    val bundle = Bundle()
+        bundle.putString(ITEM_KEY, item.id)
+
+        //fragment.arguments = bundle
+        fragment.setArguments(bundle)
+*/
+
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.gif_frame, gifFragment)
+                .replace(R.id.animated_drawable_frame, animatedDrawableFragment)
+                .replace(R.id.animated_vector_frame, animatedVectorFragment)
+                .commit()
+
+
+    }
+}
