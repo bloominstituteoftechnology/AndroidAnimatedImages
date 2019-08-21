59706e70e275032fb83a01a6ec155c1c3696fdfc
jDanielDetes <jdanieldetes@gmail.com>
2019-08-21 16:20:18 -0400

    Added 1st Gif now onto frame gif

--- app/src/main/java/com/example/animated_images/MainActivity.kt
+++ app/src/main/java/com/example/animated_images/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.animated_images
 
+import android.annotation.SuppressLint
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("WrongThread")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+        val angry_kid_drawable =
+            ImageDecoder.decodeDrawable(ImageDecoder.createSource(resources, R.drawable.angry_kid))
+            animated_image.setImageDrawable(angry_kid_drawable)
+
+            (angry_kid_drawable as AnimatedImageDrawable).start()
+
+
+        }
     }
 }
